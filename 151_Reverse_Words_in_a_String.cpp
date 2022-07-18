28382663e23fc0eec5319c403db747251f4b6737
Yash Gaherwar <72980929+yashgaherwar@users.noreply.github.com>
2022-07-18 21:26:37 +0530

    Create 151_Reverse_Words_in_a_String.cpp

---
+++ 151_Reverse_Words_in_a_String.cpp
@@ -0,0 +1,77 @@
+/*
+
+Given an input string s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+Example 1:
+
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+
+Example 2:
+
+Input: s = "  hello world  "
+Output: "world hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+
+Example 3:
+
+Input: s = "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+
+Constraints:
+
+1 <= s.length <= 104
+s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+There is at least one word in s.
+
+*/
+
+/* Solution */
+
+class Solution {
+public:
+  
+    // Stack Approach.
+    // Time Complexity:- O(N)
+    // Space complexity:- O(N)
+  
+    string reverseWords(string s) {
+        stack<string> st;
+        string ans="";
+        string res="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]==' '){
+                if(ans!=""){
+                  st.push(ans);  
+                }
+                
+                ans="";
+            }
+            else{
+                ans+=s[i];
+            }
+        }
+        
+        if(ans!=""){
+             st.push(ans);
+        }
+       
+        while(!st.empty()){
+            res+=st.top();  
+            st.pop();
+            if(!st.empty()){
+              res+=" ";  
+            }
+            
+        }
+        
+        return res;
+    }
+};
