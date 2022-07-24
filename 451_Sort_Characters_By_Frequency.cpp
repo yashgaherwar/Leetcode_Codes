b11ca54513e9856fb8b9d16d9ccccae1378a2d17
Yash Gaherwar <72980929+yashgaherwar@users.noreply.github.com>
2022-07-24 14:32:15 +0530

    Create 451_Sort_Characters_By_Frequency.cpp

---
+++ 451_Sort_Characters_By_Frequency.cpp
@@ -0,0 +1,68 @@
+/*
+
+Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+
+Return the sorted string. If there are multiple answers, return any of them.
+
+Example 1:
+
+Input: s = "tree"
+Output: "eert"
+Explanation: 'e' appears twice while 'r' and 't' both appear once.
+So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+
+Example 2:
+
+Input: s = "cccaaa"
+Output: "aaaccc"
+Explanation: Both 'c' and 'a' appear three times, so both "cccaaa" and "aaaccc" are valid answers.
+Note that "cacaca" is incorrect, as the same characters must be together.
+
+Example 3:
+
+Input: s = "Aabb"
+Output: "bbAa"
+Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
+Note that 'A' and 'a' are treated as two different characters.
+ 
+
+Constraints:
+
+1 <= s.length <= 5 * 105
+s consists of uppercase and lowercase English letters and digits.
+
+*/
+
+/* Solution */
+
+// Map and Vector Pair Approach
+
+class Solution {
+public:
+    static bool cmp(pair<char,int>& a,pair<char,int>& b){
+            return a.second < b.second;
+    }
+    
+    string frequencySort(string s) {
+        map<char,int> mp;
+        
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]++;
+        }
+        
+        vector<pair<char,int>> vp(mp.begin(),mp.end());
+        sort(vp.begin(),vp.end(),cmp);
+        
+        for(auto i: mp){
+            cout<<i.first<<endl;
+        }
+        string ans="";
+        for(auto i: vp){
+            while(i.second--){
+                ans.push_back(i.first);
+            }
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
