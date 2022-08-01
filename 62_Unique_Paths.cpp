30b42d8fa2a9cbb8d6fc2063d33c0b664092693e
Yash Gaherwar <72980929+yashgaherwar@users.noreply.github.com>
2022-08-01 23:01:59 +0530

    Create 62_Unique_Paths.cpp

---
+++ 62_Unique_Paths.cpp
@@ -0,0 +1,58 @@
+/*
+
+There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+Example 1:
+
+Input: m = 3, n = 7
+Output: 28
+
+Example 2:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+ 
+
+Constraints:
+
+1 <= m, n <= 100
+
+*/
+
+/* Solution */
+
+class Solution {
+public:
+    
+   // Dynamic Programming 
+   // Recursion + Memoization (Top Down DP)
+    
+   int fun(int i,int j,int m,int n, vector<vector<int>> &dp){
+       if(i>=m || j>=n){
+           return 0;
+       }
+       if(i==m-1 && j==n-1){
+           return 1;
+       } 
+       if(dp[i][j]!=-1){
+           return dp[i][j];
+       }
+       
+       return dp[i][j]=fun(i+1,j,m,n,dp)+fun(i,j+1,m,n,dp);
+       
+       
+   }
+    
+   int uniquePaths(int m, int n) {
+       vector<vector<int>> dp(m,vector<int> (n,-1));
+       return fun(0,0,m,n,dp);
+   } 
+    
