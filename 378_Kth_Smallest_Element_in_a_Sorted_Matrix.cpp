5f972e562fc6431f2857d7541323ea85362e0cb3
Yash Gaherwar <72980929+yashgaherwar@users.noreply.github.com>
2022-08-02 19:17:40 +0530

    Update 378_Kth_Smallest_Element_in_a_Sorted_Matrix.cpp

--- 378_Kth_Smallest_Element_in_a_Sorted_Matrix.cpp
+++ 378_Kth_Smallest_Element_in_a_Sorted_Matrix.cpp
@@ -13,12 +13,12 @@ Example 1:
 Input: matrix = [[1,5,9],[10,11,13],[12,13,15]], k = 8
 Output: 13
 Explanation: The elements in the matrix are [1,5,9,10,11,12,13,13,15], and the 8th smallest number is 13
+
 Example 2:
 
 Input: matrix = [[-5]], k = 1
 Output: -5
  
-
 Constraints:
 
 n == matrix.length == matrix[i].length
