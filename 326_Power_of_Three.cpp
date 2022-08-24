25a300d18a942211ae2584c5deca94324c1a576e
Yash Gaherwar <72980929+yashgaherwar@users.noreply.github.com>
2022-08-24 17:41:17 +0530

    Create 326_Power_of_Three.cpp

---
+++ 326_Power_of_Three.cpp
@@ -0,0 +1,48 @@
+/*
+
+Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+Example 1:
+
+Input: n = 27
+Output: true
+
+Example 2:
+
+Input: n = 0
+Output: false
+
+Example 3:
+
+Input: n = 9
+Output: true
+ 
+Constraints:
+
+-2^31 <= n <= 2^31 - 1
+
+*/
+
+/* Solution */
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<=0){
+            return false;
+        }
+        
+        long long ans=pow(3,(ceil)(log(n)/log(3)));
+        //int ans=pow(3,x);
+        
+        if(ans==n){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
+ 
