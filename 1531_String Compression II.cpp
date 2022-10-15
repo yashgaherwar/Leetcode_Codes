1913154535ecb98f79b4cfb959307bbad5b7843a
Yash Gaherwar <72980929+yashgaherwar@users.noreply.github.com>
2022-10-15 21:12:40 +0530

    Create 1531_String Compression II.cpp

---
+++ 1531_String Compression II.cpp
@@ -0,0 +1,67 @@
+/*
+
+Run-length encoding is a string compression method that works by replacing consecutive identical characters (repeated 2 or more times) with the concatenation of the character and the number marking the count of the characters (length of the run). For example, to compress the string "aabccc" we replace "aa" by "a2" and replace "ccc" by "c3". Thus the compressed string becomes "a2bc3".
+
+Notice that in this problem, we are not adding '1' after single characters.
+
+Given a string s and an integer k. You need to delete at most k characters from s such that the run-length encoded version of s has minimum length.
+
+Find the minimum length of the run-length encoded version of s after deleting at most k characters.
+
+Example 1:
+
+Input: s = "aaabcccd", k = 2
+Output: 4
+Explanation: Compressing s without deleting anything will give us "a3bc3d" of length 6. Deleting any of the characters 'a' or 'c' would at most decrease the length of the compressed string to 5, for instance delete 2 'a' then we will have s = "abcccd" which compressed is abc3d. Therefore, the optimal way is to delete 'b' and 'd', then the compressed version of s will be "a3c3" of length 4.
+
+Example 2:
+
+Input: s = "aabbaa", k = 2
+Output: 2
+Explanation: If we delete both 'b' characters, the resulting compressed string would be "a4" of length 2.
+
+Example 3:
+
+Input: s = "aaaaaaaaaaa", k = 0
+Output: 3
+Explanation: Since k is zero, we cannot delete anything. The compressed string is "a11" of length 3.
+
+Constraints:
+
+1 <= s.length <= 100
+0 <= k <= s.length
+s contains only lowercase English letters.
+
+*/
+
+/* Solution */
+
+class Solution {
+public:
+    int getLength(int cnt) {
+        return cnt == 1 ? 1 : (cnt < 10 ? 2 : (cnt < 100 ? 3 : 4));
+    }
+    int getLengthOfOptimalCompression(string s, int k) {
+        int n = s.size();
+        int dp[n+1][k+1];
+        for(int i = n; i >= 0; i--) {
+            for(int j = 0; j <= k; j++) {
+                if(i == n){
+                    dp[n][j] = 0; continue;
+                }
+                dp[i][j] = j > 0 ? dp[i+1][j-1] : INT_MAX;
+                int max_del = j, cnt = 0;
+                for (int i_end = i; i_end < n && max_del >= 0; i_end++) {
+                    if(s[i_end] == s[i]){
+                        cnt++;
+                        dp[i][j] = min(dp[i][j], getLength(cnt) + dp[i_end+1][max_del]);
+                    } 
+                    else{
+                        max_del--;
+                    }
+                }
+            }
+        }
+        return dp[0][k];
+    }
+};
