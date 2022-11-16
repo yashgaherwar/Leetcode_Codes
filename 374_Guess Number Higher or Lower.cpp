816dfc99413180cf0a7152092faaaeb98c71bf2c
Yash Gaherwar <72980929+yashgaherwar@users.noreply.github.com>
2022-11-16 09:04:07 +0530

    Create 374_Guess Number Higher or Lower.cpp

---
+++ 374_Guess Number Higher or Lower.cpp
@@ -0,0 +1,77 @@
+/*
+
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
+
+Example 1:
+
+Input: n = 10, pick = 6
+Output: 6
+
+Example 2:
+
+Input: n = 1, pick = 1
+Output: 1
+
+Example 3:
+
+Input: n = 2, pick = 1
+Output: 1
+ 
+Constraints:
+
+1 <= n <= 2^31 - 1
+1 <= pick <= n
+
+*/
+
+/* Solution */
+
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+
+    // Binary Search Approach
+
+    int guessNumber(int n) {
+        int l=0;
+        int r=n;
+        int ans;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+
+            int check = guess(mid);
+
+            if(check==-1){
+                r = mid-1;
+            }
+            else if(check==1){
+                l = mid+1;;
+            }
+            else{
+                ans = mid;
+                break;
+            }
+        }
+
+        return ans;
+    }
+};
