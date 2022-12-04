accf3ef90628792b6b042cc742b3d4c45ae41f83
Yash Gaherwar <72980929+yashgaherwar@users.noreply.github.com>
2022-12-04 11:04:35 +0530

    Create 2256_Minimum_Average_Difference.cpp

---
+++ 2256_Minimum_Average_Difference.cpp
@@ -0,0 +1,79 @@
+/*
+
+You are given a 0-indexed integer array nums of length n.
+
+The average difference of the index i is the absolute difference between the average of the first i + 1 elements of nums and the average of the last n - i - 1 elements. Both averages should be rounded down to the nearest integer.
+
+Return the index with the minimum average difference. If there are multiple such indices, return the smallest one.
+
+Note:
+
+The absolute difference of two numbers is the absolute value of their difference.
+The average of n elements is the sum of the n elements divided (integer division) by n.
+The average of 0 elements is considered to be 0.
+ 
+Example 1:
+
+Input: nums = [2,5,3,9,5,3]
+Output: 3
+Explanation:
+- The average difference of index 0 is: |2 / 1 - (5 + 3 + 9 + 5 + 3) / 5| = |2 / 1 - 25 / 5| = |2 - 5| = 3.
+- The average difference of index 1 is: |(2 + 5) / 2 - (3 + 9 + 5 + 3) / 4| = |7 / 2 - 20 / 4| = |3 - 5| = 2.
+- The average difference of index 2 is: |(2 + 5 + 3) / 3 - (9 + 5 + 3) / 3| = |10 / 3 - 17 / 3| = |3 - 5| = 2.
+- The average difference of index 3 is: |(2 + 5 + 3 + 9) / 4 - (5 + 3) / 2| = |19 / 4 - 8 / 2| = |4 - 4| = 0.
+- The average difference of index 4 is: |(2 + 5 + 3 + 9 + 5) / 5 - 3 / 1| = |24 / 5 - 3 / 1| = |4 - 3| = 1.
+- The average difference of index 5 is: |(2 + 5 + 3 + 9 + 5 + 3) / 6 - 0| = |27 / 6 - 0| = |4 - 0| = 4.
+The average difference of index 3 is the minimum average difference so return 3.
+
+Example 2:
+
+Input: nums = [0]
+Output: 0
+Explanation:
+The only index is 0 so return 0.
+The average difference of index 0 is: |0 / 1 - 0| = |0 - 0| = 0.
+ 
+
+Constraints:
+
+1 <= nums.length <= 10^5
+0 <= nums[i] <= 10^5
+
+*/
+
+/* Solution */
+
+#define ll long long
+class Solution {
+public:
+    int minimumAverageDifference(vector<int>& nums) {
+        ll n=nums.size();
+        ll mini=INT_MAX,idx=-1;
+        vector<ll> pref(n,0);
+
+        for(int i=0;i<n;i++){
+            pref[i]=nums[i]+((i==0)?0:pref[i-1]);
+        }
+
+        for(int i=0;i<n;i++){
+            ll val1=pref[i];
+            ll val2=pref[n-1]-pref[i];
+            ll diff;
+            if(i == n-1){
+               diff = val1/(i+1);
+            }
+            else{
+               diff = abs(val1/(i+1)-val2/(n-i-1)); 
+            }
+            
+
+            if(diff<mini){
+                mini=diff;
+                idx=i;
+            }     
+        }
+
+        return idx;
+        
+    }
+};
