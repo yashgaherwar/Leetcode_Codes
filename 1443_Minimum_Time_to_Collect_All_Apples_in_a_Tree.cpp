cfbe53484c4203c3330118a1dd2bb8d8768eeb90
Yash Gaherwar <72980929+yashgaherwar@users.noreply.github.com>
2023-01-11 23:03:11 +0530

    Create 1443_Minimum_Time_to_Collect_All_Apples_in_a_Tree.cpp

---
+++ 1443_Minimum_Time_to_Collect_All_Apples_in_a_Tree.cpp
@@ -0,0 +1,68 @@
+/*
+
+Given an undirected tree consisting of n vertices numbered from 0 to n-1, which has some apples in their vertices. You spend 1 second to walk over one edge of the tree. Return the minimum time in seconds you have to spend to collect all apples in the tree, starting at vertex 0 and coming back to this vertex.
+
+The edges of the undirected tree are given in the array edges, where edges[i] = [ai, bi] means that exists an edge connecting the vertices ai and bi. Additionally, there is a boolean array hasApple, where hasApple[i] = true means that vertex i has an apple; otherwise, it does not have any apple.
+
+Example 1:
+
+Input: n = 7, edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]], hasApple = [false,false,true,false,true,true,false]
+Output: 8 
+Explanation: The figure above represents the given tree where red vertices have an apple. One optimal path to collect all apples is shown by the green arrows. 
+
+Example 2:
+
+Input: n = 7, edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]], hasApple = [false,false,true,false,false,true,false]
+Output: 6
+Explanation: The figure above represents the given tree where red vertices have an apple. One optimal path to collect all apples is shown by the green arrows.  
+
+Example 3:
+
+Input: n = 7, edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]], hasApple = [false,false,false,false,false,false,false]
+Output: 0
+ 
+Constraints:
+
+1 <= n <= 10^5
+edges.length == n - 1
+edges[i].length == 2
+0 <= ai < bi <= n - 1
+fromi < toi
+hasApple.length == n
+
+*/
+
+/* Solution */
+
+class Solution {
+public:
+    int minimumTimeToCollectApples(int idx, vector<int> adj[],vector<bool> &hasApple,int par){
+        int path_length = 0;
+
+        for(auto i: adj[idx]){
+            if(i!=par){
+                int flag=minimumTimeToCollectApples(i,adj,hasApple,idx);
+                if(hasApple[i]){
+                    path_length+=(2+flag);
+                    hasApple[idx]=true;
+                }
+            }
+        }
+        
+        
+        return path_length;
+    }
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
+        vector<int> adj[n];
+        
+        for(int i=0;i<edges.size();i++){
+            
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
+            
+        }
+        
+        return minimumTimeToCollectApples(0,adj,hasApple,0);
+
+    }
+};
