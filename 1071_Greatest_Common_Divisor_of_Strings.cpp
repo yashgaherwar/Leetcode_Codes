2ed2a5b5dd92477863c91ede6ae14ca8ddf4e10d
Yash Gaherwar <72980929+yashgaherwar@users.noreply.github.com>
2023-02-01 10:04:29 +0530

    Create 1071_Greatest_Common_Divisor_of_Strings.cpp

---
+++ 1071_Greatest_Common_Divisor_of_Strings.cpp
@@ -0,0 +1,43 @@
+/*
+
+For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
+
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+Example 1:
+
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+
+Example 2:
+
+Input: str1 = "ABABAB", str2 = "ABAB"
+Output: "AB"
+
+Example 3:
+
+Input: str1 = "LEET", str2 = "CODE"
+Output: ""
+ 
+Constraints:
+
+1 <= str1.length, str2.length <= 1000
+str1 and str2 consist of English uppercase letters.
+
+*/
+
+/* Solution */
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if(str1+str2 != str2+str1){
+            return "";
+        }
+
+        int m=str1.size(),n=str2.size();
+        int res=__gcd(m,n);
+
+        return str2.substr(0,res);
+    }
+};
