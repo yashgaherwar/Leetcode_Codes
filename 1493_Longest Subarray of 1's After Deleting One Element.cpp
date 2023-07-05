0eb2ecf7856901364fbc8ccdfa19d6bed040485c
Yash Gaherwar <72980929+yashgaherwar@users.noreply.github.com>
2023-07-05 21:10:48 +0530

    Create 1493_Longest Subarray of 1's After Deleting One Element.cpp

---
+++ 1493_Longest Subarray of 1's After Deleting One Element.cpp
@@ -0,0 +1,64 @@
+/*
+
+Given a binary array nums, you should delete one element from it.
+
+Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
+
+Example 1:
+
+Input: nums = [1,1,0,1]
+Output: 3
+Explanation: After deleting the number in position 2, [1,1,1] contains 3 numbers with value of 1's.
+  
+Example 2:
+
+Input: nums = [0,1,1,1,0,1,1,0,1]
+Output: 5
+Explanation: After deleting the number in position 4, [0,1,1,1,1,1,0,1] longest subarray with value of 1's is [1,1,1,1,1].
+  
+Example 3:
+
+Input: nums = [1,1,1]
+Output: 2
+Explanation: You must delete one element.
+ 
+
+Constraints:
+
+1 <= nums.length <= 10^5
+nums[i] is either 0 or 1.
+
+*/
+
+/* Solution */
+
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int l=0,r=0,cnt=0,maxi=INT_MIN;
+
+        while(r<nums.size()){
+            if(nums[r]==0){
+                cnt++;
+            }
+            if(cnt<=1){
+                maxi=max(maxi,r-l);
+                r++;
+            }
+            else{
+                while(cnt>1){
+                    if(nums[l]==0){
+                        cnt--;
+                    }
+                    l++;
+                }
+                maxi=max(maxi,l-r);
+                r++;
+
+            }
+        }
+
+        return maxi;
+
+    }
+};
