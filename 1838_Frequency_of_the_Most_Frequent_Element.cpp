3249a9b5b791d842775e7d9a915244d3969cf9b4
Yash Gaherwar <72980929+yashgaherwar@users.noreply.github.com>
2023-11-18 21:10:51 +0530

    Create 1838_Frequency_of_the_Most_Frequent_Element.cpp

---
+++ 1838_Frequency_of_the_Most_Frequent_Element.cpp
@@ -0,0 +1,63 @@
+/*
+
+The frequency of an element is the number of times it occurs in an array.
+
+You are given an integer array nums and an integer k. In one operation, you can choose an index of nums and increment the element at that index by 1.
+
+Return the maximum possible frequency of an element after performing at most k operations.
+
+Example 1:
+
+Input: nums = [1,2,4], k = 5
+Output: 3
+Explanation: Increment the first element three times and the second element two times to make nums = [4,4,4].
+4 has a frequency of 3.
+
+Example 2:
+
+Input: nums = [1,4,8,13], k = 5
+Output: 2
+Explanation: There are multiple optimal solutions:
+- Increment the first element three times to make nums = [4,4,8,13]. 4 has a frequency of 2.
+- Increment the second element four times to make nums = [1,8,8,13]. 8 has a frequency of 2.
+- Increment the third element five times to make nums = [1,4,13,13]. 13 has a frequency of 2.
+
+Example 3:
+
+Input: nums = [3,9,6], k = 2
+Output: 1
+  
+Constraints:
+
+1 <= nums.length <= 10^5
+1 <= nums[i] <= 10^5
+1 <= k <= 10^5
+
+
+*/
+
+/* Solution */
+
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        long long n = nums.size(), sum = 0;
+        long long i=0, j=0;
+
+        sort(nums.begin(),nums.end());
+
+        while(j<n){
+            sum += nums[j];
+
+            if(((j-i+1)*nums[j] - sum) > k){
+                sum -= nums[i];
+                i++;
+            }
+
+            j++;
+        }
+
+        return j-i;
+
+    }
+};
