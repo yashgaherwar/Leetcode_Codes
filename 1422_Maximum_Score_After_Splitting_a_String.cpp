8cfef7efc7dbfa95cf7fb030e548d63ddeb3b470
Yash Gaherwar <72980929+yashgaherwar@users.noreply.github.com>
2023-12-22 16:16:41 +0530

    Create 1422_Maximum_Score_After_Splitting_a_String.cpp

---
+++ 1422_Maximum_Score_After_Splitting_a_String.cpp
@@ -0,0 +1,88 @@
+/*
+
+Given a string s of zeros and ones, return the maximum score after splitting the string into two non-empty substrings (i.e. left substring and right substring).
+
+The score after splitting a string is the number of zeros in the left substring plus the number of ones in the right substring.
+
+Example 1:
+
+Input: s = "011101"
+Output: 5 
+Explanation: 
+All possible ways of splitting s into two non-empty substrings are:
+left = "0" and right = "11101", score = 1 + 4 = 5 
+left = "01" and right = "1101", score = 1 + 3 = 4 
+left = "011" and right = "101", score = 1 + 2 = 3 
+left = "0111" and right = "01", score = 1 + 1 = 2 
+left = "01110" and right = "1", score = 2 + 1 = 3
+  
+Example 2:
+
+Input: s = "00111"
+Output: 5
+Explanation: When left = "00" and right = "111", we get the maximum score = 2 + 3 = 5
+  
+Example 3:
+
+Input: s = "1111"
+Output: 3
+  
+Constraints:
+
+2 <= s.length <= 500
+The string s consists of characters '0' and '1' only.
+
+*/
+
+/* Solution */
+
+class Solution {
+public:
+    int maxScore(string s) {
+        int n = s.size();
+        vector<int> pref(n), suff(n);
+
+        if(s[0] == '0'){
+            pref[0]=1;
+        }
+        else{
+            pref[0]=0;
+        }
+
+        for(int i=1;i<n;i++){
+            if(s[i] == '0'){
+                pref[i]=1+pref[i-1];
+            }
+            else{
+                pref[i]=pref[i-1];
+            }
+        }
+
+        if(s[n-1] == '1'){
+            suff[n-1]=1;
+        }
+        else{
+            suff[n-1]=0;
+        }
+
+        for(int i=n-2;i>=0;i--){
+            if(s[i] == '1'){
+                suff[i]=suff[i+1]+1;
+            }
+            else{
+                suff[i]=suff[i+1];
+            }
+        }
+
+        int ans = 0;
+
+        
+        for(int i=0;i<n-1;i++){
+            ans = max(ans, suff[i+1] + pref[i]);
+        }
+        
+        return ans;
+
+
+    }
+};
