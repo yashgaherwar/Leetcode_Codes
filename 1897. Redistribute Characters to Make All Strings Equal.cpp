0a5ff5ff851fd6aaa2256e784a36f432dae0d59f
Yash Gaherwar <72980929+yashgaherwar@users.noreply.github.com>
2023-12-30 10:32:29 +0530

    Create 1897. Redistribute Characters to Make All Strings Equal.cpp

---
+++ 1897. Redistribute Characters to Make All Strings Equal.cpp
@@ -0,0 +1,53 @@
+/*
+
+You are given an array of strings words (0-indexed).
+
+In one operation, pick two distinct indices i and j, where words[i] is a non-empty string, and move any character from words[i] to any position in words[j].
+
+Return true if you can make every string in words equal using any number of operations, and false otherwise.
+
+Example 1:
+
+Input: words = ["abc","aabc","bc"]
+Output: true
+Explanation: Move the first 'a' in words[1] to the front of words[2],
+to make words[1] = "abc" and words[2] = "abc".
+All the strings are now equal to "abc", so return true.
+
+Example 2:
+
+Input: words = ["ab","a"]
+Output: false
+Explanation: It is impossible to make all the strings equal using the operation.
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 100
+words[i] consists of lowercase English letters.
+
+*/
+
+/* Solution */
+
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+        map<char,int> mp;
+
+        for(auto str: words){
+            for(int j=0;j<str.size();j++){
+                mp[str[j]]++;
+            }
+        }
+
+        for(auto i: mp){
+            if(i.second % words.size() != 0){
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+};
